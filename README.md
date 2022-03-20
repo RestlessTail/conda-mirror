14f2c3bb3d6fc52dd323584b50b3be11ed073756
RestlessTail <1826930551@qq.com>
2022-03-20 13:34:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# conda-mirror
+最近conda的镜像不太稳定，我自己之前也一直被镜像的问题折磨，大概是用的人太多，有时候就很慢。
+所以我写了一个自动检测conda镜像源的响应速度并进行切换的脚本，应该会方便一些。
+
+## 使用方法
+先下载下来，一共两个文件
+
+```shell
+git clone https://github.com/RestlessTail/conda-mirror.git
+```
+
+然后运行
+
+```shell
+python3 conda-mirror.py
+```
+
+就好了
---
+++ conda-mirror.py
@@ -0,0 +1,51 @@
+import os
+import json
+def ClearChannel():
+    os.system("conda config --remove-key channels")
+
+def SetChannel(channelList):
+    for i in channelList:
+        os.system("conda config --add channels " + i)
+
+def DetechDelay(ip, count):
+    delay = os.popen("ping " + ip + " -c " + str(count) + " | grep ^rtt | cut -d \" \" -f 4 | cut -d \"/\" -f 2", "r")
+    return float(delay.read())
+
+def ReadConfig(filename):
+    f = open(filename, "r")
+    obj = json.loads(f.read())
+    f.close()
+    return obj
+
+def DelayLED(time):
+    if(time < 10.0):
+        coloredTime = "[●○○]"
+    elif(time < 20.0):
+        coloredTime = "[●●○]"
+    else:
+        coloredTime = "[●●●]"
+    return coloredTime
+
+AllMirrors = ReadConfig("config.json")
+
+index = 0
+shortestDelay = 100000.0
+shortestDelayIndex = 0
+os.system("clear")
+for i in AllMirrors:
+    index = index + 1
+    delay = DetechDelay(i["detect"], 1)
+    print(str(index) + " " + i["name"] + ": " + str(delay) + "ms " + DelayLED(delay))
+    if(delay < shortestDelay):
+        shortestDelay = delay
+        shortestDelayIndex = index
+
+sel = input("请选择镜像（默认为" + str(shortestDelayIndex) + "）：")
+if(sel == ""):
+    print("默认选择最快的镜像。")
+    sel = str(shortestDelayIndex)
+if(int(sel) < index):
+    print("清除原有的镜像。")
+    ClearChannel()
+    print("设置新镜像。")
+    SetChannel(AllMirrors[int(sel)]["mirrors"])
